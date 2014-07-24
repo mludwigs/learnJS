44e1517d78e1c81c000d1531e2ec65e57a2fe80c
Matt Ludwigs <matthew.ludwigs@meltmedia.com>
2014-07-24 00:16:31 -0700

    stuff: master

--- proto/script.js
+++ proto/script.js
@@ -1,45 +1,21 @@
 function Human() {
 
-  this.legs = 2;
-
 }
 
-// Solider Class
+// Soldier class
 function Soldier(name) {
   this.name = name;
-  this.type = "soldier"
-}
-
-Soldier.prototype = new Human();
-
-Soldier.prototype = {
-  gun: '',
-
-  shoot: function () {
-   if (this.type === "soldier") {
-     console.log('bang');
-   } else if (this.type === "gunner") {
-     console.log('Rapid fire bang bang');
-   } else {
-     console.log(this.type);
-   }
-  }
+  Human.call(this);
 };
 
-// Gunner Class
+// Soldier.prototype = new Human();
+
+
+// Gunner class
 function Gunner(name) {
-  this.type = "gunner";
+  Soldier.call(this, name);
 }
 
-Gunner.prototype = new Soldier();
+var g = new Gunner('hello');
 
-Gunner.prototype = {
-  gun: 'Machine Gun'
-};
-
-var soldier = new Soldier('matt');
-var gunner = new Gunner('bill');
-
-soldier.shoot();
-console.log(gunner.name);
-
+console.log(g.name);
